14263febd37d13a3114f081d4935732079360028
camilohr.dev <chernandez@unimayor.edu.co>
2024-07-17 20:12:00 -0500

    Fix Checkstyle errors

--- src/main/java/org/devsoft/storemanagement/StoreManagementApplication.java
+++ src/main/java/org/devsoft/storemanagement/StoreManagementApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StoreManagementApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(StoreManagementApplication.class, args);
 	}
-
 }
